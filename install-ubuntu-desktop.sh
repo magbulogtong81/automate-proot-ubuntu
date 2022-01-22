b190aa065517e638596fcff8b21d39c48dd588e2
Jil Magbulogtong <magbulogtong81@gmail.com>
2022-01-23 01:04:37 +0800

    Update install-ubuntu-desktop.sh

--- install-ubuntu-desktop.sh
+++ install-ubuntu-desktop.sh
@@ -8,6 +8,7 @@ pkg update && pkg upgrade -y
 pkg install proot proot-distro -y
 
 #install ubuntu
+proot-distro install ubuntu
 printf "\033[;32mINFO\033[0m [going to proot environment]\n"
 proot-distro login ubuntu --termux-home << EOF
 bash packages.sh
